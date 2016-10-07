84e1c6a86cb524f0d132360ed3e4c55d32857c9b
Luke Hedger <luke.hedger@gmail.com>
2016-10-07 11:22:26 +0100

    tidy

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const electron = require('electron');
+const electron = require('electron')
 // Module to control application life.
-const {app} = electron;
+const { app } = electron
 // Module to create native browser window.
-const {BrowserWindow} = electron;
+const { BrowserWindow } = electron
 
 // live reload w/hard reset
 if (process.env.NODE_ENV === 'development') {
@@ -24,14 +24,14 @@ mb.on('ready', function ready () {
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win;
+let win
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600, icon: './static/img/cliff-icon.png' });
+  win = new BrowserWindow({width: 800, height: 600, icon: './static/img/cliff-icon.png' })
 
   // and load the index.html of the app.
-  win.loadURL(`file://${__dirname}/index.html`);
+  win.loadURL(`file://${__dirname}/index.html`)
 
   // Open the DevTools.
   if (process.env.NODE_ENV === 'development') {
@@ -46,28 +46,28 @@ function createWindow() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null;
-  });
+    win = null
+  })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow();
+    createWindow()
   }
 })
