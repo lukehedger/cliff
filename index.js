d8dde19ce05499fd3de4b91999c5898a8994361e
Luke Hedger <luke.hedger@gmail.com>
2016-10-10 16:19:19 +0100

    shrink

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ let win
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600, icon: './static/img/cliff-icon.png' })
+  win = new BrowserWindow({width: 400, height: 300, icon: './static/img/cliff-icon.png' })
 
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/index.html`)
