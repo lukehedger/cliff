a74278ff224fc844a51d522c8edadf124d98761b
Luke Hedger <luke.hedger@gmail.com>
2016-10-11 10:31:52 +0100

    stop creating window in prod mode and remove cat icon!!

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const electron = require('electron')
 const { app, BrowserWindow } = electron
 
+const isDevelopment = process.env.NODE_ENV === 'development'
+
 // live reload w/hard reset
-if (process.env.NODE_ENV === 'development') {
+if (isDevelopment) {
   require('electron-reload')(__dirname, {
     electron: require('electron-prebuilt')
   })
@@ -12,7 +14,7 @@ if (process.env.NODE_ENV === 'development') {
 const menubar = require('menubar')
 
 // init menubar
-const mb = menubar({ icon: './static/img/icon.png' })
+const mb = menubar({ icon: `${__dirname}/static/img/icon.png` })
 
 mb.on('ready', function ready () {
   console.log('app is ready')
@@ -24,16 +26,18 @@ mb.on('ready', function ready () {
 let win
 
 function createWindow() {
+
+  // only for debugging
+  if (!isDevelopment) return
+
   // Create the browser window.
-  win = new BrowserWindow({width: 400, height: 300, icon: './static/img/cliff-icon.png' })
+  win = new BrowserWindow({width: 400, height: 300, icon: `${__dirname}/static/img/icon.png` })
 
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/index.html`)
 
   // Open the DevTools.
-  if (process.env.NODE_ENV === 'development') {
-    win.webContents.openDevTools()
-  }
+  win.webContents.openDevTools()
 
   // init React DevTools
   // BrowserWindow.addDevToolsExtension('~/Library/Application\ Support/Google/Chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/0.14.11_0')
